f65e07c947ac07c0319f1102a86242c9fcac4ae5
elg256 <133916011+Elg256@users.noreply.github.com>
2025-04-10 20:38:46 +0200

    Update hmac.py

--- cryptcrro/hmac.py
+++ cryptcrro/hmac.py
@@ -1,5 +1,6 @@
 import hashlib
 
+BLOCK_SIZE = 64 * 1024
 
 def xor_bytes(a_b: bytes, b_b: bytes) -> bytes:
     return bytes(a ^ b for a, b in zip(a_b, b_b))
@@ -19,3 +20,22 @@ def hmac_sha256(secret: bytes, message: bytes) -> bytes:
         .digest()
 
     return final_hash
+
+def hmac_sha256_chunk(secret: bytes):
+    ipad = b"\x36" * 64
+    opad = b"\x5c" * 64
+
+    if len(secret) > 64:
+        secret = hashlib.sha256(secret).digest()
+    secret = secret.ljust(64, b'\x00')
+
+    si = xor_bytes(secret, ipad)
+    so = xor_bytes(secret, opad)
+
+    inner = hashlib.sha256()
+    inner.update(si)
+
+    outer = hashlib.sha256()
+    outer.update(so)
+
+    return inner, outer
