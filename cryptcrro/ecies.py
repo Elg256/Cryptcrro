a6763a99f0ad55e86074a639446d619c0e2b2e44
elg256 <133916011+Elg256@users.noreply.github.com>
2025-04-23 14:01:57 +0200

    Update ecies.py

--- cryptcrro/ecies.py
+++ cryptcrro/ecies.py
@@ -3,6 +3,7 @@ import base64
 import hashlib
 from cryptcrro.utility import insert_newlines_with_tags
 from cryptcrro import _rust
+from cryptcrro.symetric import crro as scrro
 
 generator_point = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
 
@@ -13,13 +14,11 @@ def generate_public_key(private_key:int):
     return _rust.generate_public_key(int(private_key))
 
 
-def encrypt_symetric(symetric_key, message):
-    return bytes(_rust.chacha20_encrypt(symetric_key, message.hex()))
+def encrypt_symetric(symetric_key, message) -> str:
+    return scrro.encrypt(symetric_key, message).decode()
 
-
-def decrypt_symetric(symetric_key, encrypted_message):
-    return bytes.fromhex(_rust.chacha20_decrypt(symetric_key,
-                                                           base64.urlsafe_b64decode(encrypted_message).hex()))
+def decrypt_symetric(symetric_key, encrypted_message) -> bytes:
+    return scrro.decrypt(symetric_key, encrypted_message)
 
 def encrypt_message(public_key, message):
 
@@ -30,9 +29,8 @@ def encrypt_message(public_key, message):
     shared_secret = _rust.ecies_mul_points(random_int, public_key)
 
     gx_shared, gy_shared = shared_secret
-    gx_shared = str(gx_shared)
 
-    symetric_key = hashlib.sha256(gx_shared.encode('utf-8')).digest()
+    symetric_key = hashlib.sha256(gx_shared.to_bytes(32, 'big')).digest()
 
     encrypted_message = encrypt_symetric(symetric_key ,message)
 
@@ -76,9 +74,8 @@ def decrypt_message(private_key, encrypted_message):
     shared_secret = _rust.ecies_mul_points(private_key, random_int_on_curve)
 
     gx_shared, gy_shared = shared_secret
-    gx_shared = str(gx_shared)
 
-    symetric_key = hashlib.sha256(gx_shared.encode('utf-8')).digest()
+    symetric_key = hashlib.sha256(gx_shared.to_bytes(32, 'big')).digest()
 
     decrypted_message = decrypt_symetric(symetric_key, encrypted_message)
 
